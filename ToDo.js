1d903805a11dcec08de5c0d20efe42cc059dfaf6
mohandand <mohandand@gmail.com>
2022-05-30 14:15:21 -0700

    2 files modified

--- ToDo.js
+++ ToDo.js
@@ -17,18 +17,38 @@ export default function ToDo() {
     setTasks([...temp]);
   }
 
+  function deleteTask(e) {
+    e.preventDefault();
+    let temp = tasks;
+    temp.splice(e.target.id, 1);
+    setTasks([...temp]);
+  }
+
   let tasksList = tasks.map((item, idx) => {
-    return <div key={idx}>{item}</div>;
+    return (
+      <div key={idx}>
+        {item}
+        <button id={idx} onClick={deleteTask}>
+          X
+        </button>
+      </div>
+    );
   });
 
   return (
     <div className="container">
-      <h2> TODO APP</h2>
-      <input className="input" onChange={getValue}></input>
-      <button className="add" onClick={getTasks}>
-        Add
-      </button>
-      <div className="dislaytasks">{tasksList}</div>
+      <div className="appcomponents">
+        <div>
+          <h2> TODO APP</h2>
+        </div>
+        <div>
+          <input className="input" onChange={getValue}></input>
+          <button className="add" onClick={getTasks}>
+            Add
+          </button>
+        </div>
+        <div className="dislaytasks">{tasksList}</div>
+      </div>
     </div>
   );
 }
